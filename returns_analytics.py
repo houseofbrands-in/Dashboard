5fe76bf40f28bdd55d11217900b25ad92ecff541
RJ <rupesh.jhawar@gmail.com>
2025-12-11 19:43:41 +0530

    returns_analytics.py

---
+++ returns_analytics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+class ReturnsAnalytics:
+    @staticmethod
+    def build_returns_insights(returns_df, params):
+        """Equivalent to BuildReturnsInsights VBA"""
+        pass
+    
+    @staticmethod 
+    def build_returns_type_split(returns_df, master_df, params):
+        """Equivalent to BuildReturnsTypeSplit VBA"""
+        pass
